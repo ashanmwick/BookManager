ac02cdf95df97a0276f01b3d57ea7dd77a96a175
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-12-29 14:43:47 +0530

    Dbcontext

--- BookManager/obj/Debug/net8.0/BookManager.AssemblyInfo.cs
+++ BookManager/obj/Debug/net8.0/BookManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3ef2048fe9b84d9894102b80b20734b9ee433b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f2864103ec7dc7ec06432b56e93743cb23c9e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
